e264d034822acb4e85b01eea0d95cc3065586fb4
Joseph Chen <jchen42703@gmail.com>
2023-05-02 17:39:08 -0400

    feat: basic motion detection

--- demo.py
+++ demo.py
@@ -1,3 +1,60 @@
+import time
+
+
+def detect_motion(fgbg, motion_detected, motion_start_time, motion_stop_time, human_w_h_ration=1.25, min_contour_area=500, counter=0):
+    event_id = 1
+
+    # Apply the background subtraction
+    fgmask = fgbg.apply(frame)
+
+    # Apply morphological opening to remove noise
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN,
+                              np.ones((3, 3), np.uint8))
+
+    # Find the contours in the binary image
+    contours, _ = cv2.findContours(
+        fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Find the largest contour
+    largest_contour = None
+    largest_contour_area = 0
+    for contour in contours:
+        contour_area = cv2.contourArea(contour)
+        if contour_area > min_contour_area and contour_area > largest_contour_area:
+            largest_contour = contour
+            largest_contour_area = contour_area
+
+    # If motion is detected
+    if largest_contour is not None:
+        # event_id = 1
+        # Draw a bounding box around the largest contour
+        (_, _, w, h) = cv2.boundingRect(largest_contour)
+        if h >= human_w_h_ration * w:
+            event_id = 2
+
+        # If motion was not detected previously, set the motion start time
+        if not motion_detected:
+            motion_detected = True
+            motion_start_time = time.time()
+
+        # Update motion stop time to current time
+        motion_stop_time = time.time()
+
+        return motion_detected, motion_start_time, motion_stop_time, event_id
+    # If no motion is detected
+    else:
+        event_id = 1
+        # If motion was detected previously and it has been more than 3 seconds, reset the motion detected flag
+        if motion_detected and (time.time() - motion_start_time) > 3:
+            motion_detected = False
+            motion_start_time = time.time()
+
+    return motion_detected, motion_start_time, motion_stop_time, event_id
+    # # If motion has not been detected for 5 seconds / if motion stop time has not been updated, exit the loop
+    # if time.time() - motion_stop_time > 5:
+    #     return motion_detected, motion_start_time, motion_stop_time, event_id
+
+
 if __name__ == "__main__":
     import cv2
     import numpy as np
@@ -14,7 +71,12 @@ if __name__ == "__main__":
     model = create_shufflenet(weights_path)
     # Initialize the webcam for Hand Gesture Recognition Python project
     cap = cv2.VideoCapture(0)
+    fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 
+    # Initialize variables for motion detection
+    motion_detected = False
+    motion_start_time = None
+    motion_stop_time = 0
     input_dim = 112
     try:
         i = 0
@@ -27,6 +89,11 @@ if __name__ == "__main__":
                 frame, reshape_size=(input_dim, input_dim))
             gathered_img[i % 16] = reshaped
             if i != 0 and i % 16 == 0:
+                motion_detected, motion_start_time, motion_stop_time, event_id = detect_motion(fgbg, motion_detected,
+                                                                                               motion_start_time=motion_start_time,
+                                                                                               motion_stop_time=motion_stop_time,
+                                                                                               counter=i)
+                print("event id: ", event_id)
                 input_tensor = preprocess_mobilenetv2_queued(gathered_img)
                 pred = model(input_tensor)
                 pred_label = int(torch.argmax(pred))
