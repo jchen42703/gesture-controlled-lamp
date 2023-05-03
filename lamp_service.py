5e08a4bef575ceea2596bf57c191e4d3f37e2fdb
Joseph Chen <jchen42703@gmail.com>
2023-05-03 17:54:41 -0400

    feat: make brightness changes additive

--- lamp_service.py
+++ lamp_service.py
@@ -25,8 +25,7 @@ class LampService:
             return OPERATIONS[2]
 
     def increase_brightness(self):
-        new_brightness = self.brightness * \
-            1.1 if self.brightness > 0 else 0.1
+        new_brightness = self.brightness + 0.1 if self.brightness > 0 else 0.1
         new_brightness = min(1, new_brightness)
         self.brightness = new_brightness
         print("Increaseing brightness to ", new_brightness)
@@ -34,8 +33,8 @@ class LampService:
                                         new_brightness, True)
 
     def decrease_brightness(self):
-        new_brightness = self.brightness * 0.9
-        self.brightness = new_brightness
+        new_brightness = self.brightness - 0.1
+        self.brightness = max(new_brightness, 0)
         print("Decreasing brightness to ", new_brightness)
         self.lamp_driver.set_lamp_state(self.default_hue, self.default_saturation,
                                         new_brightness, True)
